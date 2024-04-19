3179b4e47a34869304138fd47130fe109344add7
Annarhysa Albert <annarhysa13@gmail.com>
2024-04-19 17:51:15 +0530

    started with finetuning

--- csv_converter.py
+++ csv_converter.py
@@ -1,5 +1,7 @@
 import pyarrow.parquet as pq
+import pandas as pd
 
+"""
 path = input("Enter the json file path: ")
 data = pq.read_table(path)
 
@@ -7,3 +9,10 @@ df = data.to_pandas()
 
 csv_save_path = input("Enter the path where you want to save the converted csv file along with the file name: ")
 df.to_csv(csv_save_path)
+
+"""
+df = pd.read_csv("data/python_code_instructions.csv")
+
+jsonl_save_path = "data/python_code_instructions.jsonl"
+
+df.to_json(jsonl_save_path, orient='records', lines=True)
